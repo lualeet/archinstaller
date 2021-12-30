24fa45bd67705a2ee634f12398d8867da6e6305c
lualeet <43719546+lualeet@users.noreply.github.com>
2021-12-30 16:11:31 +0200

    add colors

--- linuxsetup-chroot.sh
+++ linuxsetup-chroot.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+source scripts/colors
 
 if ! [ -z $1 ] && [ $1 = "-q" ]; then
 	quiet=true
@@ -17,14 +18,14 @@ if [ -z $installdisk ]; then
 fi;
 
 if ! [ -z $quiet ] && [ $installdisk = "placeholder"]; then
-	echo " -> if running in quiet mode, a path to disk device is required."
+	echo "${error}  -> ${nc}if running in quiet mode, a path to disk device is required."
 	exit 1
 fi;
 
 if [ $installdisk = placeholder ]; then
 	installdisk=$1
 	if [ -z $installdisk ]; then
-		echo " -> please provide disk device you're installing to as argument"
+		echo "${error}  -> ${nc}please provide disk device you're installing to as argument"
 		exit 1
 	fi;
 fi;
@@ -46,51 +47,51 @@ qqread() {
 	read $1
 }
 
-echo " -> setting timezone"
+echo "${status}  -> ${nc}setting timezone"
 hwclock --systohc
 
-echo " -> generating locale"
+echo "${status}  -> ${nc}generating locale"
 locale-gen
 
-printf " => Create non root user? "
+printf "${interact}==> {$nc}Create non root user? "
 qread nonroot y
 if [ $nonroot = "n" ]; then
 	true
 else
-	printf " => Username: "
+	printf "${interact}==> {$nc}Username: "
 	qqread username user
 	if [ -z $username ]; then
 		username=user
 	fi;
 	if id $username &>/dev/null; then
-		echo " -> user $username already exists, skipping"
+		echo "${status}  -> ${nc}user $username already exists, skipping"
 	else
 		useradd -m $username
-		echo " => Password: "
+		echo "${interact}==> {$nc}Password: "
 		if [ -z $veryquiet ]; then
 			passwd $username
 		else
-			echo " -> SETTING USER \"$username\" PASSWORD TO \"password\""
+			echo "${attention}  -> ${nc}setting user \"${gold}$username${nc}\" password to \"${gold}password${nc}\""
 			echo "password" | passwd $username --stdin
 			sleep 2
 		fi;
 	fi;
 fi;
 
-echo " -> adding to sudoers"
+echo "${status}  -> ${nc}adding to sudoers"
 sed -i "s/root ALL=(ALL) ALL/root ALL=(ALL) ALL\n$username ALL=(ALL) ALL/" /etc/sudoers
 
-echo " -> installing grub as bootloader"
+echo "${status}  -> ${nc}installing grub as bootloader"
 grub-install --target=i386-pc $installdisk
-echo " -> removing quiet from grub linux parameters"
+echo "${status}  -> ${nc}removing quiet from grub linux parameters"
 sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3\"/" /etc/default/grub
 #cat /etc/default/grub | sed "s/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3\"/" > /etc/default/grub
-echo " -> making grub config"
+echo "${status}  -> ${nc}making grub config"
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo " -> chroot setup done"
+echo "${status}  -> ${nc}chroot setup done"
 
-echo " -> running scripts"
+echo "${status}  -> ${nc}running scripts"
 source scripts/main
 
-echo " -> exiting"
+echo "${status}  -> ${nc}exiting"
--- linuxsetup-livecd.sh
+++ linuxsetup-livecd.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 set -e
+source scripts/colors
 
 if [ -z $TMUX ] && [ -f /usr/bin/tmux ]; then
-	exec tmux new-session sh -c "`pwd`/linuxsetup-livecd.sh \"$@\""
+	exec tmux new-session sh -c "sh `pwd`/linuxsetup-livecd.sh \"$@\""
 fi;
 
 if ! [ -z $1 ] && [ $1 = "-q" ]; then
 	quiet=true
 	if [ -z $2 ]; then
-		echo " -> if running in quiet mode, a path to disk device is required."
+		echo "${status}  -> ${nc}if running in quiet mode, a path to disk device is required."
 		exit 1
 	fi;
 	installdisk=$2
@@ -17,14 +18,14 @@ fi;
 if ! [ -z $1 ] && [ $1 = "-qq" ]; then
 	if [ -z $2 ]; then
 		if [ -z $3 ]; then
-			echo " -> if running in quiet mode, a path to a disk device and a root password are required."
+			echo "${status}  -> ${nc}if running in quiet mode, a path to a disk device and a root password are required."
 			exit 1
 		fi;
-		echo " -> if running in quiet mode, a path to disk device is required."
+		echo "${status}  -> ${nc}if running in quiet mode, a path to disk device is required."
 		exit 1
 	fi;
 	if [ -z $3 ]; then
-		echo " -> if running in quiet mode, a root password is also required."
+		echo "${status}  -> ${nc}if running in quiet mode, a root password is also required."
 		exit 1
 	fi;
 	quiet=true
@@ -50,19 +51,19 @@ qqread() {
 	read $1
 }
 
-stat ./linuxsetup-chroot.sh 2>/dev/null >/dev/null || sh -c "echo \" -> linuxsetup-chroot.sh has not been found in current dir, you'll have to copy it manually\"; sleep 2; echo \" -> proceeding anyways\""
+stat ./linuxsetup-chroot.sh 2>/dev/null >/dev/null || sh -c "echo \"${status}  -> ${nc}linuxsetup-chroot.sh has not been found in current dir, you'll have to copy it manually\"; sleep 2; echo \" -> proceeding anyways\""
 
 err_alreadymounted_umountfail() {
-	echo " -> failed to umount"
+	echo "${status}  -> ${nc}failed to umount"
 	exit 1
 }
 err_alreadymounted() {
-	printf " => /mnt is already mounted, umount and continue? "
+	printf "${interact}==> ${nc}/mnt is already mounted, umount and continue? "
 	qread unmount y
 	if [ $unmount = "y" ]; then
 		umount /mnt || err_alreadymounted-umountfail
 	else
-		echo " -> idk what to do then"
+		echo "${status}  -> ${nc}idk what to do then"
 		exit 1
 	fi;
 }
@@ -71,56 +72,56 @@ if mountpoint -q /mnt; then
 fi;
 
 if ! stat /etc/pacman.conf -c ""; then
-	echo " -> making pacman.conf backup at /etc/pacman.conf.backup"
+	echo "${status}  -> ${nc}making pacman.conf backup at /etc/pacman.conf.backup"
 	cp /etc/pacman.conf /etc/pacman.conf.backup
 else
-	echo " -> /etc/pacman.conf.backup already exists, not making backup"
+	echo "${status}  -> ${nc}/etc/pacman.conf.backup already exists, not making backup"
 fi;
-echo " -> enabling pacman Color and ParallelDownloads"
+echo "${status}  -> ${nc}enabling Color and ParallelDownloads in pacman.conf"
 sed -i "s/#Color/Color/" /etc/pacman.conf
 sed -i "s/#ParallelDownloads/ParallelDownloads/" /etc/pacman.conf
 if ! [ -s /etc/pacman.conf ]; then
-	echo " -> /etc/pacman.conf is now empty. why"
+	echo "${status}  -> ${nc}/etc/pacman.conf is now empty. why"
 	exit 1
 fi;
 #cat /etc/pacman.conf | sed "s/#Color/Color/" | sed "s/#ParallelD/ParallelD/" > /etc/pacman.conf
 
 err_updatefailed() {
-	printf " => Failed system update, continue anyways? "
+	printf "${error}==> ${nc}Failed system update, continue anyways? "
 	qread keepgoing y;
 	if [ $keepgoing = "y" ]; then
-		echo " -> proceeding"
+		echo "${status}  -> ${nc}proceeding"
 	else
 		exit 1
 	fi;
 }
-echo " -> updating system"
+echo "${status}  -> ${nc}updating system"
 pacman -Syu --noconfirm || err_updatefailed
 
 fdisk -l | grep -i /dev/sd
-printf " => Choose disk to partition (/dev/sdX): "
+printf "${interact}==> ${nc}Choose disk to partition (${gold}/dev/sdX${nc}): "
 qread installdisk $installdisk
 installdisk2="$installdisk""2"
 
 if ! stat $installdisk -c ""; then
-	echo " -> $installdisk is not a valid file"
+	echo "${status}  -> ${nc}${gold}$installdisk${nc} is not a valid file"
 fi;
 
 fdisk -l $installdisk
 
-printf " => Partition disk \"$installdisk\"? "
+printf "${interact}==> ${nc}Partition disk \"${gold}$installdisk${nc}\"? "
 qread tocontinue y
 if [ $tocontinue = "y" ]; then
-	echo " -> proceeding"
+	echo "${status}  -> ${nc}proceeding"
 else
-	echo " -> quitting"
+	echo "${error}  -> ${nc}quitting"
 	exit 0
 fi;
 
-printf " => Partition automatically with parted (y) or manually with fdisk (n)? "
+printf "${interact}==> ${nc}Partition automatically with${green} parted (y) ${nc}or manually with fdisk (n)? "
 qread useparted y
 if [ $useparted = "y" ]; then
-	echo " -> using parted"
+	echo "${status}  -> ${nc}using parted"
 	parted $installdisk ---pretend-input-tty <<EOF
 mktable gpt
 Yes
@@ -130,49 +131,49 @@ mkpart primary 3MiB 100%
 print
 quit
 EOF
-	echo " -> done"
+	echo "${done}  -> ${nc}done"
 elif [ $useparted = "n" ]; then
-	echo " -> using fdisk"
+	echo "${status}  -> ${nc}using fdisk"
 	fdisk $installdisk
 else
-	echo " -> invalid response, expected (y/n)"
+	echo "${error}  -> ${nc}invalid response, expected (y/n)"
 	exit 1
 fi;
 
-printf " => Format $installdisk2 with ext4? "
+printf "${interact}==> ${nc}Format${gold} $installdisk2 ${nc}with ext4? "
 qread ext4 y
 if [ $ext4 = "y" ]; then
 	mkfs.ext4 -F $installdisk2
 elif [ $ext4 = "n" ]; then
-	echo " -> ^D or exit when you're done formatting"
+	echo "${attention}  -> ${nc}^D or exit when you're done formatting"
 	zsh || bash || sh
 else
-	echo " -> invalid response, expected (y/n)"
+	echo "${error}  -> ${nc}invalid response, expected (y/n)"
 	exit 1
 fi;
 
 if ! mountpoint -q /mnt; then
-	echo " -> mounting $installdisk2 to /mnt/"
+	echo "${status}  -> ${nc}mounting${gold} $installdisk2 ${nc}to /mnt/"
 	mount $installdisk2 /mnt
 else
-	echo " -> /mnt already mounted, proceeding"
+	echo "${status}  -> ${nc}/mnt already mounted, proceeding"
 fi;
 
 pacstrap /mnt base base-devel linux linux-firmware
-printf " => Install extra terminal software? (neovim, tmux) "
+printf "${interact}==> ${nc}Install extra terminal software? (neovim, tmux) "
 qread extras y
 if [ $extras = "y" ]; then
 	pacstrap /mnt neovim tmux
 fi;
 
-printf " => Install NetworkManager? "
+printf "${interact}==> ${nc}Install NetworkManager? "
 qread networkmanager y
 if [ $networkmanager = "y" ]; then
 	pacstrap /mnt networkmanager
 fi;
 
 prompt_installcustom() {
-	printf " => Install custom software? (space separated list of packages, empty for none) "
+	printf "${interact}==> ${nc}Install custom software? (space separated list of packages, empty for none) "
 	qread customs
 	if ! [ -z $customs ]; then
 		pacstrap /mnt $customs || prompt_installcustom
@@ -180,61 +181,62 @@ prompt_installcustom() {
 }
 prompt_installcustom
 
-echo " -> genfstab -U /mnt >> /mnt/etc/fstab"
+echo "${status}  -> ${nc}genfstab -U /mnt >> /mnt/etc/fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 cat /mnt/etc/fstab
-printf " => Does this fstab file seem correct? "
+printf "${interact}==> ${nc}Does this fstab file seem correct? "
 qread fstabhealthy y
 if [ $fstabhealthy = "n" ]; then
 	pacman -S neovim --needed --noconfirm
 	nvim /mnt/etc/fstab
 fi;
 
-echo " -> preparing timezone to Europe/Vilnius (Lithuania)"
+echo "${status}  -> ${nc}preparing timezone to Europe/Vilnius (Lithuania)"
 ln -sf /mnt/usr/share/zoneinfo/Europe/Vilnius /mnt/etc/localtime
 
-echo " -> preparing locale to en_US.UTF-8"
+echo "${status}  -> ${nc}preparing locale to en_US.UTF-8"
 echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 
-printf " => Your hostname: "
+printf "${interact}==> ${nc}Your hostname: "
 qqread hostname hostname
 if [ -z $hostname ]; then
 	hostname=hostname
 fi;
 echo $hostname > /mnt/etc/hostname
 
-echo " => Root password:"
+echo "${interact}==> ${nc}Root password:"
 if ! [ -z $veryquiet ]; then
-	echo " -> in very quiet mode, setting password to $rootpass"
+	echo "${attention}  -> ${nc}in very quiet mode, setting password to $rootpass"
 	echo "root:$rootpass" | chpasswd -R /mnt
+	sleep 1;
 else
 	passwd
 fi;
 
-echo " -> installing grub"
+echo "${status}  -> ${nc}installing grub"
 pacstrap /mnt grub --needed --noconfirm
 
-echo " -> copying chroot setup script"
-stat ./linuxsetup-chroot.sh 2>/dev/null >/dev/null || echo " -> linuxsetup-chroot.sh was not found in current dir, please locate it and copy it to /mnt/"
+echo "${status}  -> ${nc}copying chroot setup script"
+stat ./linuxsetup-chroot.sh 2>/dev/null >/dev/null || echo "${attention}  -> ${nc}linuxsetup-chroot.sh was not found in current dir, please locate it and copy it to /mnt/"
 cp ./linuxsetup-chroot.sh /mnt/linuxsetup-chroot.sh || true
 if [ -f /mnt/linuxsetup-chroot.sh ]; then
 	sanitizedinstalldisk=$(echo $installdisk | sed "s:/:\\\/:g")
 	sed -i "s/installdisk=placeholder/installdisk=$sanitizedinstalldisk/" /mnt/linuxsetup-chroot.sh
 	#cat /mnt/linuxsetup-chroot.sh | sed "s/installdisk=placeholder/installdisk="$installdisk"/" > /mnt/linuxsetup-chroot.sh
 fi;
-echo " -> copying config scripts"
+echo "${status}  -> ${nc}copying config scripts"
 cp -r ./scripts /mnt/
 
-echo " -> wait for the partition to unmount"
+echo "${status}  -> ${nc}wait for the partition to unmount (this can take a while)"
 umount $installdisk2
 
 mount $installdisk2 /mnt
 
-echo " -> livecd setup done, run"
+echo "${status}  -> ${nc}livecd setup done, run"
 echo "   arch-chroot /mnt"
 echo "   /linuxsetup-chroot.sh"
-echo " => Exiting "
+echo "${interact}==> ${nc}Press enter to exit "
 read
 
